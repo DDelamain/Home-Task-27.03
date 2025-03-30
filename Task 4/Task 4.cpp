5a7b4ec8e081b7170bf851c575cc48b82859c546
DDelamain <doroshenkodanila1@gmail.com>
2025-03-30 20:33:52 +0500

    Add files via upload

---
+++ Task 4/Task 4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int a,i;
+int main(){
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите число a от 1 до 20: ";
+    cin >> a;
+    if (a < 1 || a>20) {
+        cout << "\nВведено некорректное число!\n";
+        return 0;
+    }
+    i = a;
+    if (i == 20) {
+        cout << "\nСумма от а до 20: " << a;
+    }
+    else {
+        while (i < 20) {
+            i++;
+            a += i;
+        }
+        cout << "\nСумма от а до 20: " << a;
+    }
+}
---
+++ Task 5/Task 5.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int k, i = 1;
+int main(){
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите ваш вариант: ";
+    cin >> k;
+    while (i != 10) {
+        cout << k << " * " << i << " = " << k * i << endl;
+        i++;
+    }
+}
